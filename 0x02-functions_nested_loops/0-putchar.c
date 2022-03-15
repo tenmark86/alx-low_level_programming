3a896e04dff33bdeac6a228c712d3c03efc6c99f
tenmark86 <tenmark86@gmail.com>
2022-03-15 04:37:15 -0700

    added 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,3 @@
-#include <unistd.h>
 #include "main.h"
 
 /**
@@ -8,14 +7,14 @@
  */
 int main(void)
 {
-        int str[] = {95,112,117,116,99,104,97,114};
-        int count, sz;
+	int str[] = {72, 111, 108, 98, 101, 114, 116, 111, 110};
+	int count, sz;
 
-        sz = sizeof(str) / sizeof(int);
-        for (count = 0; count < sz; count++)
-        {
-                _putchar(str[count]);
-        }
-        _putchar('\n');
-        return (0);
+	sz = sizeof(str) / sizeof(int);
+	for (count = 0; count < sz; count++)
+	{
+		_putchar(str[count]);
+	}
+	_putchar('\n');
+	return (0);
 }
