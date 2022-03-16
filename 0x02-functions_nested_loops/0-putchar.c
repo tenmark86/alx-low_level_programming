e578f22069f72dca2d722f5d709d6d7bb1c2a3a9
tenmark86 <tenmark86@gmail.com>
2022-03-16 04:08:32 -0700

    added 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,20 @@
-#include <unistd.h>
 #include "main.h"
 
 /**
- * main - Prints main as a message.
- *
- * Return: Always 0 (Success)
+ * main -prints _putchar, followed by a new line
+ * Return :Always 0 (success)
  */
 int main(void)
 {
-	int str[] = {72, 111, 108, 98, 101, 114, 116, 111, 110};
-	int count, sz;
-
-	sz = sizeof(str) / sizeof(int);
-	for (count = 0; count < sz; count++)
-	{
-		_putchar(str[count]);
-	}
+	_putchar(');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 }
