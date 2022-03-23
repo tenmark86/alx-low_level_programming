0237167caacfdafff4afa2e3c268065bda453966
tenmark86 <tenmark86@gmail.com>
2022-03-23 09:26:06 -0700

    added 100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,7 +1,6 @@
-#include <stddef.h>
+#include "main.h"
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+
 
 /**
 * ROT13 is a simple letter substitution cipher that replaces a letter with the letter 13 letters after it in the alphabet.
