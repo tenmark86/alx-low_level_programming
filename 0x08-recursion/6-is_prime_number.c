645045febe641b3e6c9e570e0fe36bc62db3e497
tenmark86 <tenmark86@gmail.com>
2022-03-30 03:30:40 -0700

    added update 6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,34 +1,47 @@
 i#include "main.h"
 
 /**
- * is_prime - detects if an input number is a prime number.
- * @n: input number.
- * @c: iterator.
- * Return: 1 if n is a prime number. 0 if n is not a prime number.
+ * helper - This function helps calculate whether n is prime
+ * @n: The integer to test
+ * @i: The integer to help calculate whether n is prime
+ * Return: 1 if n is prime, otherwise 0
  */
-int is_prime(unsigned int n, unsigned int c)
+
+int helper(int n, int i)
 {
-	if (n % c == 0)
+	/* if n is greater than or equal to 25 */
+	if (n >= (i * i))
 	{
-		if (n == c)
-			return (1);
-		else
+		/* skip it's multiples and even numbers after 5 */
+		if (n % i == 0 || n % (i + 2) == 0)
 			return (0);
+		/* check primes after 11 */
+		i = i + 6;
 	}
-	return (0 + is_prime(n, c + 1));
+	return (1);
 }
+
+
+
 /**
- * is_prime_number - detects if an input number is a prime number.
- * @n: input number.
- * Return: 1 if n is a prime number. 0 if n is not a prime number.
+ * is_prime_number - This function returns a 1 if input integer is prime
+ * @n: The integer to test
+ * Return: 1 if the n is prime, otherwise 0
  */
+
 int is_prime_number(int n)
 {
-	if (n == 0)
+	/* if less than 1, not a prime */
+	if (n <= 1)
 		return (0);
-	if (n < 0)
+	/* 2 and 3 are prime */
+	else if (n <= 3)
+		return (1);
+	/* if factors of 2 or 3, not likely a prime */
+	else if (n % 2 == 0 || n % 3 == 0)
 		return (0);
-	if (n == 1)
-		return (0);
-	return (is_prime(n, 2));
+	/* check everything after 5 */
+	else
+		return (helper(n, 5)
 }
+
