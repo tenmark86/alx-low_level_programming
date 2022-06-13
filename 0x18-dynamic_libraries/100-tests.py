bed7e148b915128cff4d0eb4e6a567aca9230ab8
tenmark86 <tenmark86@gmail.com>
2022-06-13 05:41:27 -0700

    added 100-tests.py

--- 0x18-dynamic_libraries/100-tests.py
+++ 0x18-dynamic_libraries/100-tests.py
@@ -1,15 +1,11 @@
-from ctypes import *
-libCalc = CDLL("./100-operations.so")
- 
-#call C function to check connection
-libCalc.connect() 
- 
-#calling randNum() C function
-#it returns random number
-varRand = libCalc.randNum()
-print "Random Number:", varRand, type(varRand)
- 
-#calling addNum() C function
-#it returns addition of two numbers
-varAdd = libCalc.addNum(20,30)
-print "Addition : ", varAdd
+import random
+import ctypes
+
+cops = ctypes.CDLL('./100-operations.so')
+a = random.randint(-111, 111)
+b = random.randint(-111, 111)
+print("{} + {} = {}".format(a, b, cops.add(a, b)))
+print("{} - {} = {}".format(a, b, cops.sub(a, b)))
+print("{} x {} = {}".format(a, b, cops.mul(a, b)))
+print("{} / {} = {}".format(a, b, cops.div(a, b)))
+print("{} % {} = {}".format(a, b, cops.mod(a, b)))
